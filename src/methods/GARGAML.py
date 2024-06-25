0f411b257c60ec600694e1f6832577fc8ab5f88f
B-Deprez <bruno.r.deprez@gmail.com>
2024-06-25 15:02:14 +0200

    prepare for directed networks

--- src/methods/GARGAML.py
+++ src/methods/GARGAML.py
@@ -5,7 +5,10 @@ import numpy as np
 from .utils.measure_functions import measure_1_function, measure_2_function, measure_3_function
 from .utils.neighbourhood_functions import summaries_neighbourhoors_node, degree_neighbours_node, GARG_AML_nodeselection
 
-def GARG_AML_node(node, G_copy):
+def GARG_AML_node_directed(node, G_copy):
+    pass
+
+def GARG_AML_node_undirected(node, G_copy):
     G_ego_second = nx.ego_graph(G_copy, node, 2)
     
     nodes_1, nodes_2, nodes_ordered = GARG_AML_nodeselection(G_ego_second, node)
@@ -24,7 +27,15 @@ def GARG_AML_node(node, G_copy):
     measure_3 = measure_3_function(piece_1_dim, piece_2_dim, piece_3_dim, adj_full)
     
     measure = measure_2 - (measure_1 + measure_3)/2
+
     return(measure)
+
+def GARG_AML_node(node, G_copy, directed = False):
+    if directed:
+        return(GARG_AML_node_directed(node, G_copy))
+    else:
+        return(GARG_AML_node_undirected(node, G_copy))
+    
 
 def combine_GARG_AML(G_selection, measures_dict, summary_dict, neigh_degree_dict):
     degree_df = pd.DataFrame(
@@ -66,13 +77,15 @@ def combine_GARG_AML(G_selection, measures_dict, summary_dict, neigh_degree_dict
     return(GARG_AML_df)
 
 def GARG_AML(G_reduced): # The method works with a pre-processed graph. G_reduced is the graph with the degree cutoff applied.
+    directed = nx.is_directed(G_reduced)
+    
     G_degree_dict = dict(G_reduced.degree())
     
     GARG_AML_values = dict()
     
     nodes = list(G_reduced.nodes)
     for node in nodes:
-        GARG_AML_node_value = GARG_AML_node(node, G_reduced)
+        GARG_AML_node_value = GARG_AML_node(node, G_reduced, directed=directed)
         GARG_AML_values[node] = GARG_AML_node_value
         
     summaries_neighbours = dict()
