1fd036e08e1c2e6a3aaf47f8de109fde390511cc
B-Deprez <bruno.r.deprez@gmail.com>
2024-06-26 10:43:35 +0200

    directed scores

--- src/methods/GARGAML.py
+++ src/methods/GARGAML.py
@@ -2,15 +2,42 @@ import pandas as pd
 import networkx as nx
 import numpy as np
 
-from .utils.measure_functions import measure_1_function, measure_2_function, measure_3_function
+from .utils.measure_functions_undirected import measure_1_function, measure_2_function, measure_3_function
+from .utils.measure_functions_directed import *
 from .utils.neighbourhood_functions import summaries_neighbourhoors_node, degree_neighbours_node, GARG_AML_nodeselection
 
 def GARG_AML_node_directed(node, G_copy):
-    pass
+    G_ego_second = nx.ego_graph(G_copy, node, 2, undirected=True) #Use both incoming and outgoing edges
+
+    nodes_0, nodes_1, nodes_2, nodes_ordered = GARG_AML_nodeselection(G_ego_second, node)
+
+    adj_full = nx.adjacency_matrix(G_ego_second, nodelist=nodes_ordered).toarray()
+
+    size_0 = len(nodes_0)
+    size_1 = len(nodes_1)
+    size_2 = len(nodes_2)
+
+    measure_00 = measure_00_function(adj_full, size_0)
+    measure_01 = measure_01_function(adj_full, size_0, size_1)
+    measure_02 = measure_02_function(adj_full, size_0, size_1, size_2)
+    measure_10 = measure_10_function(adj_full, size_0, size_1)
+    measure_11 = measure_11_function(adj_full, size_0, size_1)
+    measure_12 = measure_12_function(adj_full, size_0, size_1, size_2)  
+    measure_20 = measure_20_function(adj_full, size_0, size_2)
+    measure_21 = measure_21_function(adj_full, size_0, size_1, size_2)
+    measure_22 = measure_22_function(adj_full, size_2)
+
+    measure_high = np.mean([measure_01, measure_12])
+    measure_low = np.mean([measure_00, measure_02, measure_10, measure_11, measure_20, measure_21, measure_22])
+
+    measure = measure_high - measure_low
+
+    return(measure)
 
 def GARG_AML_node_undirected(node, G_copy):
     G_ego_second = nx.ego_graph(G_copy, node, 2)
     
+    # nodes_ordered are the nodes ordered as node, 2nd order and 1st order neighbours
     nodes_1, nodes_2, nodes_ordered = GARG_AML_nodeselection(G_ego_second, node)
     
     adj_full = nx.adjacency_matrix(G_ego_second, nodelist=nodes_ordered).toarray()
---
+++ src/methods/utils/measure_functions_directed.py
@@ -0,0 +1,114 @@
+def measure_00_function(adj_full, size_0):
+    piece_00 = adj_full[:size_0, :size_0]
+    total_sum_00 = piece_00.sum()
+    total_size_00 = piece_00.size
+    reduced_size_00 = total_size_00 - (3 * size_0) + 2
+
+    if reduced_size_00 > 0:
+        rel_00 = total_sum_00/reduced_size_00
+    else:
+        rel_00 = 0
+
+    return rel_00
+
+def measure_01_function(adj_full, size_0, size_1):
+    piece_01 = adj_full[:size_0, size_0:size_0 + size_1]
+    total_sum_01 = piece_01.sum()
+    reduced_sum_01 = total_sum_01 - size_1 - (size_0 - 1) # Additional term since some second order neighbours can be grouped under group 0
+    total_size_01 = piece_01.size
+    reduced_size_01 = total_size_01 - size_1 - (size_0 - 1) # Additional term since some second order neighbours can be grouped under group 0
+
+    if reduced_size_01 > 0:
+        rel_01 = reduced_sum_01/reduced_size_01
+    else:
+        rel_01 = 1 #Since block only contains sure connections => full sum
+
+    return rel_01
+
+def measure_02_function(adj_full, size_0, size_1, size_2):
+    piece_02 = adj_full[:size_0, size_0 + size_1:]
+    total_sum_02 = piece_02.sum()
+    total_size_02 = piece_02.size
+    reduced_size_02 = total_size_02 - size_2
+
+    if reduced_size_02 > 0:
+        rel_02 = total_sum_02/reduced_size_02
+    else:
+        rel_02 = 0
+    
+    return rel_02
+
+def measure_10_function(adj_full, size_0, size_1):
+    piece_10 = adj_full[size_0:size_0 + size_1, :size_0]
+    total_sum_10 = piece_10.sum()
+    total_size_10 = piece_10.size
+
+    if total_size_10 > 0:
+        rel_10 = total_sum_10/total_size_10
+    else:
+        rel_10 = 0
+    
+    return rel_10
+
+def measure_11_function(adj_full, size_0, size_1):
+    piece_11 = adj_full[size_0:size_0 + size_1, size_0:size_0 + size_1]
+    total_sum_11 = piece_11.sum()
+    total_size_11 = piece_11.size
+    reduced_size_11 = total_size_11 - size_1
+
+    if reduced_size_11 > 0:
+        rel_11 = total_sum_11/reduced_size_11
+    else:
+        rel_11 = 0
+    
+    return rel_11
+
+def measure_12_function(adj_full, size_0, size_1, size_2):
+    piece_12 = adj_full[size_0:size_0 + size_1, size_0 + size_1:]
+    total_sum_12 = piece_12.sum()
+    reduced_sum_12 = total_sum_12 - size_2 # Each should be connected to at least on of the order 2 nodes
+    total_size_12 = piece_12.size
+    reduced_size_12 = total_size_12 - size_2
+
+    if reduced_size_12 > 0:
+        rel_12 = reduced_sum_12/reduced_size_12
+    else:
+        rel_12 = 1 #Since block only contains sure connections => full sum
+
+def measure_20_function(adj_full, size_0, size_2):
+    piece_20 = adj_full[-size_2:, :size_0]
+    total_sum_20 = piece_20.sum()
+    total_size_20 = piece_20.size
+    reduced_size_20 = total_size_20 - size_2
+
+    if reduced_size_20 > 0:
+        rel_20 = total_sum_20/reduced_size_20
+    else:
+        rel_20 = 0
+    
+    return rel_20
+
+def measure_21_function(adj_full, size_0, size_1, size_2):
+    piece_21 = adj_full[-size_2:, size_0:size_0 + size_1]
+    total_sum_21 = piece_21.sum()
+    total_size_21 = piece_21.size
+
+    if total_size_21 > 0:
+        rel_21 = total_sum_21/total_size_21
+    else:
+        rel_21 = 0
+    
+    return rel_21
+
+def measure_22_function(adj_full, size_2):
+    piece_22 = adj_full[-size_2:, -size_2:]
+    total_sum_22 = piece_22.sum()
+    total_size_22 = piece_22.size
+    reduced_size_22 = total_size_22 - size_2
+
+    if reduced_size_22 > 0:
+        rel_22 = total_sum_22/reduced_size_22
+    else:
+        rel_22 = 0
+    
+    return rel_22
