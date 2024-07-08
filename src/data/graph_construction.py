4aa201d3d44b59febe1ee4e20b35ad02badfeac9
B-Deprez <bruno.r.deprez@gmail.com>
2024-07-08 17:02:08 +0200

    remove self-loops

--- src/data/graph_construction.py
+++ src/data/graph_construction.py
@@ -17,5 +17,6 @@ def construct_IBM_graph(path="data/HI-Small_Trans.csv", directed=False):
     edges = zip(data["Account"], data["Account.1"])
 
     G.add_edges_from(edges)
+    G.remove_edges_from([(n, n) for n in G.nodes() if G.has_edge(n, n)])
 
     return G
