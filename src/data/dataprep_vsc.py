14e8262c0fdc91e64c16ff503e0943746dba1595
B-Deprez <bruno.r.deprez@gmail.com>
2024-07-24 08:58:08 +0200

    generalise to k pieces

--- src/data/dataprep_vsc.py
+++ src/data/dataprep_vsc.py
@@ -9,19 +9,21 @@ sys.path.append(DIR)
 import pandas as pd
 
 def main(split=False):
+    k=10 
+
     if split:
         full_data_set = pd.read_csv("data/LI-Large_Trans.csv")
 
-        n = int(len(full_data_set)/5) # We devide the dataset in 5 parts
-        for i in range(5):
-            if i == 4:
+        n = int(len(full_data_set)/k) # We devide the dataset in k parts
+        for i in range(k):
+            if i == k-1:
                 full_data_set.iloc[i*n:].to_csv("data/LI-Large_Trans_"+str(i)+".csv", index=False)
             else:
                 full_data_set.iloc[i*n:(i+1)*n].to_csv("data/LI-Large_Trans_"+str(i)+".csv", index=False)
-    
+            
     else:
         full_data_set = pd.DataFrame()
-        for i in range(5):
+        for i in range(k):
             df_piece = pd.read_csv("data/LI-Large_Trans_"+str(i)+".csv")
             full_data_set = pd.concat([full_data_set, df_piece])
         full_data_set.reset_index(drop=True, inplace=True)
