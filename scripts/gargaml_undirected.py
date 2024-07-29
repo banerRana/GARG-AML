db4b1f1a8874f23edd34f0d6160c6f77221b6674
B-Deprez <bruno.r.deprez@gmail.com>
2024-07-29 14:27:12 +0200

    finalise undirected calculations

--- scripts/gargaml_undirected.py
+++ scripts/gargaml_undirected.py
@@ -1,18 +1,39 @@
-# LOAD MODULES
-# Standard library
 import os
 import sys
-
-# NOTE: Your script is not in the root directory. We must hence change the system path
-DIR = "../"
+DIR = "./"
 os.chdir(DIR)
 sys.path.append(DIR)
 
-import networkx as nx
 import pandas as pd
 
-from src.methods.utils.measure_functions_undirected import *
 from src.data.graph_construction import construct_IBM_graph
 from src.utils.graph_processing import graph_community
-from src.methods.utils.neighbourhood_functions import GARG_AML_nodeselection
+from src.methods.GARGAML import GARG_AML_node_undirected_measures
 
+dataset = "LI-Large"
+path = "data/"+dataset+"_Trans.csv"
+directed = False
+
+G = construct_IBM_graph(path=path, directed = directed)
+G_reduced = graph_community(G, 10)
+measure_1_list = []
+measure_2_list = []
+measure_3_list = []
+
+nodes = list(G_reduced.nodes)
+
+for node in nodes:
+    measure_1, measure_2, measure_3 = GARG_AML_node_undirected_measures(node, G_reduced)
+    measure_1_list.append(measure_1)
+    measure_2_list.append(measure_2)
+    measure_3_list.append(measure_3)
+
+data_dict = {
+    "node": nodes,
+    "measure_1": measure_1_list,
+    "measure_2": measure_2_list,
+    "measure_3": measure_3_list
+}
+
+df = pd.DataFrame(data_dict)
+df.to_csv("results/"+dataset+"_GARGAML_undirected.csv", index=False)
