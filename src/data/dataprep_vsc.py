87cdd99f0145551a2731ba83036c55a8d0caa428
B-Deprez <bruno.r.deprez@gmail.com>
2024-08-01 09:53:49 +0200

    read values as str

--- src/data/dataprep_vsc.py
+++ src/data/dataprep_vsc.py
@@ -6,7 +6,7 @@ def main(split=False):
     k=10 
 
     if split:
-        full_data_set = pd.read_csv("data/LI-Large_Trans.csv")
+        full_data_set = pd.read_csv("data/LI-Large_Trans.csv", dtype = "str")
 
         n = int(len(full_data_set)/k) # We devide the dataset in k parts
         for i in range(k):
@@ -18,7 +18,7 @@ def main(split=False):
     else:
         full_data_set = pd.DataFrame()
         for i in range(k):
-            df_piece = pd.read_csv("data/LI-Large_Trans_"+str(i)+".csv")
+            df_piece = pd.read_csv("data/LI-Large_Trans_"+str(i)+".csv", dtype = "str")
             full_data_set = pd.concat([full_data_set, df_piece])
         full_data_set.reset_index(drop=True, inplace=True)
 
