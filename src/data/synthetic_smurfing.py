ba4b6807770ef9c84c0f72d56d9f37667f58014a
B-Deprez <bruno.r.deprez@gmail.com>
2025-01-15 07:53:58 +0100

    add option to determine number of smurfs

--- src/data/synthetic_smurfing.py
+++ src/data/synthetic_smurfing.py
@@ -3,7 +3,7 @@ import igraph as ig
 import matplotlib.pyplot as plt
 import random
 
-def add_smurfing_patterns_separate(graph, n):
+def add_smurfing_patterns_separate(graph, n, num_smurfs):
     for i in range(n):
         # Add smurfing pattern to the graph
 
@@ -11,40 +11,74 @@ def add_smurfing_patterns_separate(graph, n):
         num_nodes = graph.vcount()
 
         # Determine number of nodes in the smurfing pattern
-        num_nodes_mules = random.randint(2, 5) # Randomly have two to five money mules
+        try:
+            num_nodes_mules = num_smurfs[i]
+        except:
+            num_nodes_mules = random.randint(2, 5) # Randomly have two to five money mules
+
         for i in range(num_nodes_mules+2):
             graph.add_vertex(num_nodes+i)
         for i in range(num_nodes_mules):
             graph.add_edge(num_nodes, num_nodes+i+1) # Connect the source to the money mules
-            graph.add_edge(num_nodes+i+1, num_nodes+num_nodes_mules+1) # Connect the money mules to the sink
+            graph.add_edge(num_nodes+i+1, num_nodes+num_nodes_mules+1) # Connect the money mules to the target
     
     return graph
 
-def add_smurfing_patterns_new_mules(graph, n):
+def add_smurfing_patterns_new_mules(graph, n, num_smurfs):
+    for i in range(n):
+        # Add smurfing pattern to the graph
+
+        # Determine number of nodes in the graph
+        num_nodes = graph.vcount()
+
+        # Determine number of nodes in the smurfing pattern
+        try:
+            num_nodes_mules = num_smurfs[i]
+        except:
+            num_nodes_mules = random.randint(2, 5) # Randomly have two to five money mules
+        
+        # Randomly select source and target node from existing nodes
+        node_list = graph.vs.indices
+        random.shuffle(node_list)
+        source_node = node_list[0]
+        target_node = node_list[1]
+
+        for i in range(num_nodes_mules):
+            graph.add_vertex(num_nodes+i)
+        for i in range(num_nodes_mules):
+            graph.add_edge(source_node, num_nodes+i) # Connect the source to the money mules
+            graph.add_edge(num_nodes+i, target_node) # Connect the money mules to the target
+    
+    return graph
+
+
+def add_smurfing_patterns_existing_mules(graph, n, num_smurfs):
     for i in range(n):
         # Add smurfing pattern to the graph
         pass
 
-def add_smurfing_patterns_existing_mules(graph, n):
-    for i in range(n):
-        # Add smurfing pattern to the graph
-        pass
-
-def add_smurfing_patterns(graph, n, type=''):
+def add_smurfing_patterns(graph, n, num_smurfs=[] ,type_pattern=''):
     """
     Add smurfing patterns to the original graph
     :param graph: igraph.Graph object
     :param n: number of smurfing patterns to add
+    :param num_smurfs: number of smurfs to add to each smurfing pattern
     :param type: type of smurfing pattern to add (separate, new_mules, existing_mules)
     :return: graph_smurfing: igraph.Graph object with smurfing patterns added
     """
 
-    if type == 'separate':
-        graph_smurfing = add_smurfing_patterns_separate(graph, n)
-    elif type == 'new_mules':
-        graph_smurfing = add_smurfing_patterns_new_mules(graph, n)
-    elif type == 'existing_mules':
-        graph_smurfing = add_smurfing_patterns_existing_mules(graph, n)
+    assert type(num_smurfs) == list, 'Number of smurfs should be a list'
+    assert len(num_smurfs) in [0, 1, n], 'Number of smurfs should be either empty, one value or n values, with n number of smurfing patterns'
+
+    if len(num_smurfs) == 1:
+        num_smurfs = num_smurfs * n
+
+    if type_pattern == 'separate':
+        graph_smurfing = add_smurfing_patterns_separate(graph, n, num_smurfs)
+    elif type_pattern == 'new_mules':
+        graph_smurfing = add_smurfing_patterns_new_mules(graph, n, num_smurfs)
+    elif type_pattern == 'existing_mules':
+        graph_smurfing = add_smurfing_patterns_existing_mules(graph, n, num_smurfs)
     else:
         raise ValueError('Invalid type of smurfing pattern')
     
@@ -53,7 +87,7 @@ def add_smurfing_patterns(graph, n, type=''):
 if __name__ == '__main__':
     # Create synthetic Barabasi-Albert graph
     graph = ig.Graph()
-    ba = graph.Barabasi(2000,m=2)
+    ba = graph.Barabasi(20,m=2)
 
     ## Add smurfing patterns
     # We implement three types of smurfing patterns: separate, new_mules, existing_mules
@@ -61,4 +95,5 @@ if __name__ == '__main__':
     # new_mules: Smurfing patterns are constructed using new mules (which only make transactions as money mules)
     # existing_mules: Smurfing patterns are constructed using existing mules (which have made normal transactions in the past)
     # All three patterns are added to study robustness to masking
-    graph_smurfing = add_smurfing_patterns(ba, 2, type='separate')
+    graph_smurfing = add_smurfing_patterns(ba, 2, type_pattern='new_mules')
+    ig.plot(graph_smurfing, target="myfile.pdf")
