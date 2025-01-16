dc30c980797fc86c2e815e1e85a448849b776080
B-Deprez <bruno.r.deprez@gmail.com>
2025-01-16 08:05:53 +0100

    extension smurfing patterns

--- src/data/synthetic_smurfing.py
+++ src/data/synthetic_smurfing.py
@@ -14,13 +14,18 @@ def add_smurfing_patterns_separate(graph, n, num_smurfs):
         try:
             num_nodes_mules = num_smurfs[i]
         except:
-            num_nodes_mules = random.randint(2, 5) # Randomly have two to five money mules
+            num_nodes_mules = random.randint(2, 10) # Randomly have two to five money mules
 
-        for i in range(num_nodes_mules+2):
-            graph.add_vertex(num_nodes+i)
-        for i in range(num_nodes_mules):
-            graph.add_edge(num_nodes, num_nodes+i+1) # Connect the source to the money mules
-            graph.add_edge(num_nodes+i+1, num_nodes+num_nodes_mules+1) # Connect the money mules to the target
+        for j in range(num_nodes_mules+2): # Add two extra nodes for the source and target
+            graph.add_vertex(num_nodes+j)
+            graph.vs[num_nodes+j]['new_node'] = True
+            graph.vs[num_nodes+j]['laundering'] = True
+            graph.vs[num_nodes+j]['separate'] = True
+        for k in range(num_nodes_mules):
+            graph.add_edge(num_nodes, num_nodes+k+1) # Connect the source to the money mules
+            graph.es[graph.get_eid(num_nodes, num_nodes+k+1)]['laundering'] = True
+            graph.add_edge(num_nodes+k+1, num_nodes+num_nodes_mules+1) # Connect the money mules to the target
+            graph.es[graph.get_eid(num_nodes+k+1, num_nodes+num_nodes_mules+1)]['laundering'] = True
     
     return graph
 
@@ -32,22 +37,29 @@ def add_smurfing_patterns_new_mules(graph, n, num_smurfs):
         num_nodes = graph.vcount()
 
         # Determine number of nodes in the smurfing pattern
-        try:
+        if len(num_smurfs) == 0:
+            num_nodes_mules = random.randint(2, 10) # Randomly have two to five money mules
+        else:
             num_nodes_mules = num_smurfs[i]
-        except:
-            num_nodes_mules = random.randint(2, 5) # Randomly have two to five money mules
         
         # Randomly select source and target node from existing nodes
-        node_list = graph.vs.indices
-        random.shuffle(node_list)
-        source_node = node_list[0]
-        target_node = node_list[1]
+        source_node = list_nodes.pop()
+        target_node = list_nodes.pop()
+        graph.vs[source_node]['laundering'] = True
+        graph.vs[target_node]['laundering'] = True
+        graph.vs[source_node]['new_mules'] = True
+        graph.vs[target_node]['new_mules'] = True
 
-        for i in range(num_nodes_mules):
-            graph.add_vertex(num_nodes+i)
-        for i in range(num_nodes_mules):
-            graph.add_edge(source_node, num_nodes+i) # Connect the source to the money mules
-            graph.add_edge(num_nodes+i, target_node) # Connect the money mules to the target
+        for j in range(num_nodes_mules):
+            graph.add_vertex(num_nodes+j)
+            graph.vs[num_nodes+j]['new_node'] = True
+            graph.vs[num_nodes+j]['laundering'] = True
+            graph.vs[num_nodes+j]['new_mules'] = True
+        for k in range(num_nodes_mules):
+            graph.add_edge(source_node, num_nodes+k) # Connect the source to the money mules
+            graph.es[graph.get_eid(source_node, num_nodes+k)]['laundering'] = True
+            graph.add_edge(num_nodes+k, target_node) # Connect the money mules to the target
+            graph.es[graph.get_eid(num_nodes+k, target_node)]['laundering'] = True
     
     return graph
 
@@ -55,7 +67,31 @@ def add_smurfing_patterns_new_mules(graph, n, num_smurfs):
 def add_smurfing_patterns_existing_mules(graph, n, num_smurfs):
     for i in range(n):
         # Add smurfing pattern to the graph
-        pass
+
+        # Determine number of nodes in the smurfing pattern
+        if len(num_smurfs) == 0:
+            num_nodes_mules = random.randint(2, 10) # Randomly have two to five money mules
+        else:
+            num_nodes_mules = num_smurfs[i]
+    
+        # Randomly select source and target node from existing nodes
+        source_node = list_nodes.pop()
+        target_node = list_nodes.pop()
+        graph.vs[source_node]['laundering'] = True
+        graph.vs[target_node]['laundering'] = True
+        graph.vs[source_node]['existing_mules'] = True
+        graph.vs[target_node]['existing_mules'] = True
+
+        for i in range(num_nodes_mules):
+            node = list_nodes.pop()
+            graph.vs[node]['laundering'] = True
+            graph.vs[node]['existing_mules'] = True
+            graph.add_edge(source_node, node)
+            graph.es[graph.get_eid(source_node, node)]['laundering'] = True
+            graph.add_edge(node, target_node)
+            graph.es[graph.get_eid(node, target_node)]['laundering'] = True
+
+    return graph
 
 def add_smurfing_patterns(graph, n, num_smurfs=[] ,type_pattern=''):
     """
@@ -87,7 +123,19 @@ def add_smurfing_patterns(graph, n, num_smurfs=[] ,type_pattern=''):
 if __name__ == '__main__':
     # Create synthetic Barabasi-Albert graph
     graph = ig.Graph()
-    ba = graph.Barabasi(20,m=2)
+    ba = graph.Barabasi(200, m=1)
+
+    list_nodes = ba.vs.indices
+    random.shuffle(list_nodes)
+
+    ba.vs['new_node'] = False 
+    ba.vs['laundering'] = False
+
+    ba.vs['separate'] = False
+    ba.vs['new_mules'] = False
+    ba.vs['existing_mules'] = False
+
+    ba.es['laundering'] = False
 
     ## Add smurfing patterns
     # We implement three types of smurfing patterns: separate, new_mules, existing_mules
@@ -95,5 +143,41 @@ if __name__ == '__main__':
     # new_mules: Smurfing patterns are constructed using new mules (which only make transactions as money mules)
     # existing_mules: Smurfing patterns are constructed using existing mules (which have made normal transactions in the past)
     # All three patterns are added to study robustness to masking
-    graph_smurfing = add_smurfing_patterns(ba, 2, type_pattern='new_mules')
-    ig.plot(graph_smurfing, target="myfile.pdf")
+    graph_smurfing = add_smurfing_patterns(ba, 3, type_pattern='separate')
+    graph_smurfing = add_smurfing_patterns(ba, 3, type_pattern='new_mules')
+    graph_smurfing = add_smurfing_patterns(ba, 3, type_pattern='existing_mules')
+    graph_smurfing.simplify(combine_edges='max')
+
+    graph_smurfing.vs['color'] = ['red' if x['new_node'] else 'blue' for x in graph_smurfing.vs]
+    graph_smurfing.vs['size'] = [10 if x['laundering'] else 5 for x in graph_smurfing.vs]
+    graph_smurfing.es['color'] = ['black' if x['laundering'] else 'gray' for x in graph_smurfing.es]
+
+    visual_style = {}
+    visual_style["vertex_size"] = graph_smurfing.vs['size']
+    visual_style["vertex_color"] = graph_smurfing.vs['color']
+    visual_style["edge_color"] = graph_smurfing.es['color']
+    visual_style["edge_width"] = 1
+    ig.plot(graph_smurfing, **visual_style, target="data/visualisation_network.pdf")
+
+    # Extract nodes into a pandas dataframe
+    nodes_data = {attr: graph_smurfing.vs[attr] for attr in graph_smurfing.vs.attributes()}
+    nodes_df = pd.DataFrame(nodes_data)[['separate', 'new_mules', 'existing_mules', 'laundering']]
+    nodes_df.fillna(False, inplace=True)
+
+    # Extract edges into a pandas dataframe
+    source_list = []
+    target_list = []
+    for edge in graph_smurfing.es:
+        source = edge.source
+        target = edge.target
+        source_list.append(source)
+        target_list.append(target)
+    
+    edges_df = pd.DataFrame({'source': source_list, 'target': target_list})
+
+    print('Number of nodes:', graph_smurfing.vcount())
+    print('Number of edges:', graph_smurfing.ecount())
+
+    # Save the dataframe to a CSV file
+    nodes_df.to_csv('data/label_data_synthetic.csv', index=False)
+    edges_df.to_csv('data/edge_data_synthetic.csv', index=False)
