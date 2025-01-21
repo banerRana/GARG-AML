d5640ab3901a0c39361f0b2d095dc86c4518193d
B-Deprez <bruno.r.deprez@gmail.com>
2025-01-21 15:19:29 +0100

    put calculations in function

--- scripts/gargaml_undirected.py
+++ scripts/gargaml_undirected.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import time
+from functools import partial
 DIR = "./"
 os.chdir(DIR)
 sys.path.append(DIR)
@@ -16,33 +17,33 @@ from src.data.graph_construction import construct_IBM_graph, construct_synthetic
 from src.utils.graph_processing import graph_community
 from src.methods.GARGAML import GARG_AML_node_undirected_measures
 
-dataset = "synthetic"
-directed = False
-if dataset in ["HI-Small", "LI-Large"]:
-    path = "data/"+dataset+"_Trans.csv"
-    G = construct_IBM_graph(path=path, directed = directed)
-elif dataset == "synthetic":
-    path = "data/edge_data_synthetic.csv"
-    G = construct_synthetic_graph(path=path, directed = directed)
-else:
-    raise ValueError("Invalid dataset")
-
-G_reduced = graph_community(G)
-
 # Define a function to process each node
-def process_node(node):
+def process_node(node, G_reduced):
     measure_1, measure_2, measure_3, size_1, size_2, size_3 = GARG_AML_node_undirected_measures(node, G_reduced, include_size=True)
     return (node, measure_1, measure_2, measure_3, size_1, size_2, size_3)
 
-if __name__ == '__main__':
+def main():
+    dataset = "synthetic"
+    directed = False
+    if dataset in ["HI-Small", "LI-Large"]:
+        path = "data/"+dataset+"_Trans.csv"
+        G = construct_IBM_graph(path=path, directed = directed)
+    elif dataset == "synthetic":
+        path = "data/edge_data_synthetic.csv"
+        G = construct_synthetic_graph(path=path, directed = directed)
+    else:
+        raise ValueError("Invalid dataset")
+
+    G_reduced = graph_community(G)
     # Get the list of nodes
     nodes = list(G_reduced.nodes)
 
     print("Here we go")
     start_time = time.time()
     # Use multiprocessing to process nodes in parallel
+    process_node_partial = partial(process_node, G_reduced=G_reduced)
     with Pool(cpu_count()) as pool:
-        results = list(tqdm(pool.imap(process_node, nodes), total=len(nodes)))
+        results = list(tqdm(pool.imap(process_node_partial, nodes), total=len(nodes)))
 
     # Unpack the results
     nodes, measure_1_list, measure_2_list, measure_3_list, size_1_list, size_2_list, size_3_list = zip(*results)
@@ -65,3 +66,6 @@ if __name__ == '__main__':
 
     df = pd.DataFrame(data_dict)
     df.to_csv("results/"+dataset+"_GARGAML_undirected.csv", index=False)
+
+if __name__=='__main__':
+    main()
