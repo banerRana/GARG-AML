a1c4cb6348560cfb8e429046177e864a7376bd48
B-Deprez <bruno.r.deprez@gmail.com>
2025-01-21 15:20:12 +0100

    experiments synthetic data

---
+++ scripts/gargaml_directed_synth.py
@@ -0,0 +1,116 @@
+import os
+import sys
+import time
+from functools import partial
+DIR = "./"
+os.chdir(DIR)
+sys.path.append(DIR)
+
+import pandas as pd
+from tqdm import tqdm
+from multiprocessing import Pool, cpu_count
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+from src.data.graph_construction import construct_IBM_graph, construct_synthetic_graph
+from src.utils.graph_processing import graph_community
+from src.methods.GARGAML import GARG_AML_node_directed_measures
+
+def process_node(node, G_reduced, G_reduced_und, G_reduced_rev):
+    (
+        measure_00, measure_01, measure_02, measure_10, measure_11, measure_12, measure_20, measure_21, measure_22, 
+        size_00, size_01, size_02, size_10, size_11, size_12, size_20, size_21, size_22 
+    ) = GARG_AML_node_directed_measures(node, G_reduced, G_reduced_und, G_reduced_rev, include_size=True)
+    return (
+        node, 
+        measure_00, measure_01, measure_02, measure_10, measure_11, measure_12, measure_20, measure_21, measure_22, 
+        size_00, size_01, size_02, size_10, size_11, size_12, size_20, size_21, size_22
+    )
+
+def score_calculation(dataset):
+    directed = True
+    path = 'data/edge_data_'+dataset+'.csv'
+    G = construct_synthetic_graph(path=path, directed = directed)
+
+    G_reduced = graph_community(G)
+
+    G_reduced_und = G_reduced.to_undirected()
+    G_reduced_rev = G_reduced.reverse(copy=True)
+
+    nodes = list(G_reduced.nodes)
+
+    print("Here we go")
+    start_time = time.time()
+
+    process_node_partial = partial(process_node, G_reduced=G_reduced, G_reduced_und=G_reduced_und, G_reduced_rev=G_reduced_rev)
+    with Pool(cpu_count()) as pool:
+        results = list(tqdm(pool.imap(process_node_partial, nodes), total=len(nodes)))
+
+    (
+        nodes, 
+        measure_00_list, measure_01_list, measure_02_list, measure_10_list, measure_11_list, measure_12_list, measure_20_list, measure_21_list, measure_22_list, 
+        size_00_list, size_01_list, size_02_list, size_10_list, size_11_list, size_12_list, size_20_list, size_21_list, size_22_list
+    ) = zip(*results)
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+
+    print(f"Total time taken: {elapsed_time:.2f} seconds")
+
+    data_dict = {
+        "node": nodes,
+        "measure_00": measure_00_list,
+        "measure_01": measure_01_list,
+        "measure_02": measure_02_list,
+        "measure_10": measure_10_list,
+        "measure_11": measure_11_list,
+        "measure_12": measure_12_list,
+        "measure_20": measure_20_list,
+        "measure_21": measure_21_list,
+        "measure_22": measure_22_list, 
+        "size_00": size_00_list,
+        "size_01": size_01_list,
+        "size_02": size_02_list,
+        "size_10": size_10_list,
+        "size_11": size_11_list,
+        "size_12": size_12_list,
+        "size_20": size_20_list,
+        "size_21": size_21_list,
+        "size_22": size_22_list
+    }
+
+    df = pd.DataFrame(data_dict)
+    df.to_csv("results/"+dataset+"_GARGAML_directed.csv", index=False)
+
+if __name__ == '__main__':
+    n_nodes_list = [100, 10000, 100000] # Number of nodes in the graph
+    m_edges_list = [1, 2, 5] # Number of edges to attach from a new node to existing nodes
+    p_edges_list = [0.001, 0.01] # Probability of adding an edge between two nodes
+    generation_method_list = [
+        'Barabasi-Albert', 
+        'Erdos-Renyi', 
+        'Watts-Strogatz'
+        ] # Generation method for the graph
+    n_patterns_list = [3, 5] # Number of smurfing patterns to add
+    
+    for n_nodes in n_nodes_list:
+        for n_patterns in n_patterns_list:
+            if n_patterns <= 0.06*n_nodes:
+                for generation_method in generation_method_list:
+                    if generation_method == 'Barabasi-Albert':
+                        p_edges = 0
+                        for m_edges in m_edges_list:
+                            string_name = generation_method + '_'  + str(n_nodes) + '_' + str(m_edges) + '_' + str(p_edges) + '_' + str(n_patterns)
+                            score_calculation(string_name)
+                    if generation_method == 'Erdos-Renyi':
+                        m_edges = 0
+                        for p_edges in p_edges_list:
+                            string_name = generation_method + '_'  + str(n_nodes) + '_' + str(m_edges) + '_' + str(p_edges) + '_' + str(n_patterns)
+                            score_calculation(string_name)
+
+                    if generation_method == 'Watts-Strogatz':
+                        for m_edges in m_edges_list:
+                            for p_edges in p_edges_list:
+                                string_name = generation_method + '_'  + str(n_nodes) + '_' + str(m_edges) + '_' + str(p_edges) + '_' + str(n_patterns)
+                                score_calculation(string_name)
---
+++ scripts/gargaml_undirected_synth.py
@@ -0,0 +1,95 @@
+import os
+import sys
+import time
+from functools import partial
+DIR = "./"
+os.chdir(DIR)
+sys.path.append(DIR)
+
+import pandas as pd
+from tqdm import tqdm
+from multiprocessing import Pool, cpu_count
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+from src.data.graph_construction import construct_IBM_graph, construct_synthetic_graph
+from src.utils.graph_processing import graph_community
+from src.methods.GARGAML import GARG_AML_node_undirected_measures
+
+# Define a function to process each node
+def process_node(node, G_reduced):
+    measure_1, measure_2, measure_3, size_1, size_2, size_3 = GARG_AML_node_undirected_measures(node, G_reduced, include_size=True)
+    return (node, measure_1, measure_2, measure_3, size_1, size_2, size_3)
+
+def score_calculation(dataset):
+    print("====================================")
+    print(dataset)
+    directed = False
+    path = 'data/edge_data_'+dataset+'.csv'
+    G = construct_synthetic_graph(path=path, directed = directed)
+    
+    G_reduced = graph_community(G)
+    # Get the list of nodes
+    nodes = list(G_reduced.nodes)
+
+    print("Here we go")
+    start_time = time.time()
+    # Use multiprocessing to process nodes in parallel
+    process_node_partial = partial(process_node, G_reduced=G_reduced)
+    with Pool(cpu_count()) as pool:
+        results = list(tqdm(pool.imap(process_node_partial, nodes), total=len(nodes)))
+
+    # Unpack the results
+    nodes, measure_1_list, measure_2_list, measure_3_list, size_1_list, size_2_list, size_3_list = zip(*results)
+
+    # End timing
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+
+    print(f"Total time taken: {elapsed_time:.2f} seconds")
+
+    data_dict = {
+        "node": nodes,
+        "measure_1": measure_1_list,
+        "measure_2": measure_2_list,
+        "measure_3": measure_3_list,
+        "size_1": size_1_list,
+        "size_2": size_2_list,
+        "size_3": size_3_list
+    }
+
+    df = pd.DataFrame(data_dict)
+    df.to_csv("results/"+dataset+"_GARGAML_undirected.csv", index=False)
+
+if __name__ == '__main__':
+    n_nodes_list = [100, 10000, 100000] # Number of nodes in the graph
+    m_edges_list = [1, 2, 5] # Number of edges to attach from a new node to existing nodes
+    p_edges_list = [0.001, 0.01] # Probability of adding an edge between two nodes
+    generation_method_list = [
+        'Barabasi-Albert', 
+        'Erdos-Renyi', 
+        'Watts-Strogatz'
+        ] # Generation method for the graph
+    n_patterns_list = [3, 5] # Number of smurfing patterns to add
+    
+    for n_nodes in n_nodes_list:
+        for n_patterns in n_patterns_list:
+            if n_patterns <= 0.06*n_nodes:
+                for generation_method in generation_method_list:
+                    if generation_method == 'Barabasi-Albert':
+                        p_edges = 0
+                        for m_edges in m_edges_list:
+                            string_name = generation_method + '_'  + str(n_nodes) + '_' + str(m_edges) + '_' + str(p_edges) + '_' + str(n_patterns)
+                            score_calculation(string_name)
+                    if generation_method == 'Erdos-Renyi':
+                        m_edges = 0
+                        for p_edges in p_edges_list:
+                            string_name = generation_method + '_'  + str(n_nodes) + '_' + str(m_edges) + '_' + str(p_edges) + '_' + str(n_patterns)
+                            score_calculation(string_name)
+
+                    if generation_method == 'Watts-Strogatz':
+                        for m_edges in m_edges_list:
+                            for p_edges in p_edges_list:
+                                string_name = generation_method + '_'  + str(n_nodes) + '_' + str(m_edges) + '_' + str(p_edges) + '_' + str(n_patterns)
+                                score_calculation(string_name)
